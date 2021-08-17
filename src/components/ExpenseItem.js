25a6a512634a8b9456329bf75b57095d1c375269
Jesus Aranda <jesusmaria.aranda@mercadolibre.com>
2021-08-17 20:20:29 -0300

    [FTR] - ExpenseItem - Created new component to represent expense a expense item - Outputting Dynamic Data - Rendering a list of item with map method:x

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const ExpenseItem = ({ date, title, amount }) => {
+  return (
+    <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
