708b3f96a07ab9f1bb9e0445457f0bf91401594d
Jesus Aranda <jesusmaria.aranda@mercadolibre.com>
2021-09-03 17:52:37 -0300

    [FTR] - ExpensesFilter Component:         - Listening to Events & Working with event handlers         - Working with useState hook         - Adding  dropdown to filter a year         - Adding two way binding         - Child to parent component communication (bottom-up)         - Lifting the state up

--- src/components/Expenses/index.js
+++ src/components/Expenses/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "../ExpenseItem";
+import ExpensesFilter from "../ExpensesFilter";
 
 import "./styles.css";
 
 const Expenses = ({ items }) => {
+  const [selectedYear, setSelectedYear] = useState("2019");
+
+  const saveSelectedYearHandler = (selectedYear) => {
+    console.log("selected year: ", selectedYear);
+    setSelectedYear(selectedYear);
+  };
   return (
     <div className="expenses">
+      <ExpensesFilter
+        selected={selectedYear}
+        onSaveSelectedYear={saveSelectedYearHandler}
+      />
       {items.map((item) => (
         <ExpenseItem key={item.id} {...item} />
       ))}
---
+++ src/components/ExpensesFilter/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.css";
+
+const ExpensesFilter = ({ onSaveSelectedYear, selected }) => {
+  const dropdownChangeHandler = (ev) => {
+    const { value } = ev.target;
+    console.log("picked date:", value);
+    // Lifting selected year up
+    onSaveSelectedYear(value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select onChange={dropdownChangeHandler} value={selected}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
